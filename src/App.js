632e7a93236d4245293db07237f720e382a66f74
alinaland <114952993+alinaland@users.noreply.github.com>
2022-11-17 12:11:26 -0500

    Added the github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a href="https://alina-ananyeva.netlify.app">Alina Ananyeva</a> and is{" "}
+        <a
+          href="https://github.com/alinaland/react-weather-app"
+          targte="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
