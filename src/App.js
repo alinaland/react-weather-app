c8b0d07b4305c62421acf12744fc6a9104f1bb77
alinaland <114952993+alinaland@users.noreply.github.com>
2022-11-17 12:24:17 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a href="https://alina-ananyeva.netlify.app">Alina Ananyeva</a> and is{" "}
-        <a
-          href="https://github.com/alinaland/react-weather-app"
-          targte="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a href="https://alina-ananyeva.netlify.app">Alina Ananyeva</a> and is{" "}
+          <a
+            href="https://github.com/alinaland/react-weather-app"
+            targte="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
