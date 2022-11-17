151f0b30fcef929b9415c4b6dd19bf5625980d7f
alinaland <114952993+alinaland@users.noreply.github.com>
2022-11-17 14:35:37 -0500

    Changed colors

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather() {
             <input
               type="submit"
               value="Search"
-              className="btn btn-primary w-100"
+              className="btn btn-light w-100"
             />
           </div>
         </div>
